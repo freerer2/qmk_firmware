509993a07509d07c4d42c7dd4e2e03c6c9c3de74
LHK <30331140+freerer2@users.noreply.github.com>
2024-03-11 13:29:57 +0900

    마우스 레이어 개선
    오른손 새끼손 하단 HOLD시 드래그 AND 마우스 레이어
    왼손    새끼손 하단 HOLD시 마우스 레이어

--- users/freerer2/freerer2.h
+++ users/freerer2/freerer2.h
@@ -99,7 +99,7 @@ LAYER_X(DEBUG,	"Debug")
 	#define LAYER_BASE \
 	KC_Q,				KC_W,				KC_E,				KC_R,				KC_T,				KC_Y,				KC_U,				KC_I,				KC_O,				KC_P,				\
 	LGUI_T(KC_A),		LALT_T(KC_S),		LCTL_T(KC_D),		LSFT_T(KC_F),		KC_G,				KC_H,				LSFT_T(KC_J),		LCTL_T(KC_K),		LALT_T(KC_L),		LGUI_T(KC_QUOT),	\
-	LT(U_MOUSE,KC_Z),	KC_X,				KC_C,				KC_V,				KC_B,				KC_N,				KC_M,				KC_COMM,			KC_DOT,				LT(U_MOUSE,KC_SLSH),\
+	LT(U_MOUSE,KC_Z),	KC_X,				KC_C,				KC_V,				KC_B,				KC_N,				KC_M,				KC_COMM,			KC_DOT,				TD(U_TD_COMM_SCRL),\
 											LT(U_DEBUG,KC_ESC),	LT(U_NAV,KC_SPC),	LT(U_FUN,KC_TAB),	LT(U_SYM,KC_ENT),	LT(U_NUM,KC_BSPC),	KC_DEL
 
 	#define LAYER_NUM \
