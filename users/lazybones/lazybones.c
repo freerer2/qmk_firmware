f5ee541b8699b0f9cb4d706f7a78d8b291410452
LHK <freerer2@gmail.com>
2024-04-07 17:57:54 +0900

    마크다운 업데이트, 키맵 구조 변경

--- users/lazybones/lazybones.c
+++ users/lazybones/lazybones.c
@@ -1,108 +1,6 @@
 #include QMK_KEYBOARD_H
 #include "lazybones.h"
 
-// 콤보 목록 나열
-enum combos {
-	#define COMBO_X(NAME, WORK_KEY, ...) NAME,
-		COMBO_LIST
-	#undef COMBO_X
-};
-
-// 콤보 키 배열 정의
-#define COMBO_X(NAME, WORK_KEY, ...) const uint16_t PROGMEM COMBO_##NAME[] = {__VA_ARGS__, COMBO_END};
-	COMBO_LIST
-#undef COMBO_X
-
-// 콤보 동작키 매핑
-combo_t key_combos[] = {
-	#define COMBO_X(NAME, WORK_KEY, ...) [NAME] = COMBO(uint16_t PROGMEM COMBO_##NAME, WORK_KEY),
-		COMBO_LIST
-	#undef COMBO_X
-};
-
-// 오버라이드 목록 나열
-enum overrides {
-	#define OVERRIDE_X(NAME, MOD, ORI, CHG) NAME,
-		OVERRIDE_LIST
-	#undef OVERRIDE_X
-};
-
-// 오버라이드 동작 정의
-#define OVERRIDE_X(NAME, MOD, ORI, CHG) const key_override_t OVERRIDE_##NAME = ko_make_basic(MOD, ORI, CHG);
-	OVERRIDE_LIST
-#undef OVERRIDE_X
-
-// 오버라이드 전역 정의
-const key_override_t **key_overrides = (const key_override_t *[]){
-	#define OVERRIDE_X(NAME, MOD, ORI, CHG) &OVERRIDE_##NAME,
-		OVERRIDE_LIST
-	#undef OVERRIDE_X
-	NULL
-};
-
-
-typedef enum {
-    TD_NONE,
-    TD_UNKNOWN,
-    TD_SINGLE_TAP,
-    TD_SINGLE_HOLD,
-	TD_DOUBLE_TAP,
-	TD_DOUBLE_HOLD,
-} td_state_t;
-
-td_state_t cur_dance(tap_dance_state_t *state) {
-    if (state->count == 1) {
-        if (!state->pressed) {return TD_SINGLE_TAP;}
-        else return TD_SINGLE_HOLD;
-    } else if (state->count == 2) return TD_DOUBLE_TAP;
-    else return TD_UNKNOWN;
-}
-
-// 유저키코드 추가
-enum my_keycodes {
-	ECLIPSE,
-	BROWSER = SAFE_RANGE,
-	NEXACRO,
-	STEP_INTO,
-	STEP_OVER,
-	STEP_OUT,
-	RESUME,
-};
-
-// 레이어추가
-enum layers {
-	#define LAYER_X(LAYER, STRING) U_##LAYER,
-		LAYER_LIST
-	#undef LAYER_X
-};
-
-// 더블탭레이어 키코드 추가 
-enum {
-    U_TD_BOOT,
-    U_TD_COMM_SCRL,
-    U_TD_BTN2_SCRL,
-	#define LAYER_X(LAYER, STRING) U_TD_##LAYER,
-		LAYER_LIST
-	#undef LAYER_X
-};
-
-// 레이어 정의
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	#define LAYER_X(LAYER, STRING) [U_##LAYER] = U_LAYER_VA_ARGS(MAPPING, LAYER_##LAYER),
-		LAYER_LIST
-	#undef LAYER_X
-};
-
-// 더블탭 레이어 함수
-#define LAYER_X(LAYER, STRING) \
-	void u_td_fn_##LAYER(tap_dance_state_t *state, void *user_data) { \
-		if (state->count == 2) { \
-			default_layer_set((layer_state_t)1 << U_##LAYER); \
-		} \
-	}
-	LAYER_LIST
-#undef LAYER_X
-
 // 더블탭 실행함수
 void u_td_fn_boot(tap_dance_state_t *state, void *user_data) {
 	if (state->count == 2) {
@@ -110,10 +8,9 @@ void u_td_fn_boot(tap_dance_state_t *state, void *user_data) {
 	}
 };
 
-static td_state_t drg_state;
-void u_td_fn_commscrl_finish(tap_dance_state_t *state, void *user_data) {
-	drg_state = cur_dance(state);
-	switch (drg_state) {
+void u_td_fn_slsh_finish(tap_dance_state_t *state, void *user_data) {
+	td_state = cur_dance(state);
+	switch (td_state) {
 		case TD_SINGLE_TAP:
 			register_code(KC_SLSH);
 			break;
@@ -131,8 +28,8 @@ void u_td_fn_commscrl_finish(tap_dance_state_t *state, void *user_data) {
     }
 }
 
-void u_td_fn_commscrl_reset(tap_dance_state_t *state, void *user_data) {
-	switch (drg_state) {
+void u_td_fn_slsh_reset(tap_dance_state_t *state, void *user_data) {
+	switch (td_state) {
 		case TD_SINGLE_TAP:
 			unregister_code(KC_SLSH);
 			break;
@@ -146,12 +43,12 @@ void u_td_fn_commscrl_reset(tap_dance_state_t *state, void *user_data) {
 		default:
 			break;
     }
-	drg_state = TD_NONE;
+	td_state = TD_NONE;
 }
 
 void u_td_fn_btn2scrl_finish(tap_dance_state_t *state, void *user_data) {
-	drg_state = cur_dance(state);
-	switch (drg_state) {
+	td_state = cur_dance(state);
+	switch (td_state) {
 		case TD_SINGLE_TAP:
 			register_code(KC_BTN2);
 			break;
@@ -171,7 +68,7 @@ void u_td_fn_btn2scrl_finish(tap_dance_state_t *state, void *user_data) {
 }
 
 void u_td_fn_btn2scrl_reset(tap_dance_state_t *state, void *user_data) {
-	switch (drg_state) {
+	switch (td_state) {
 		case TD_SINGLE_TAP:
 			unregister_code(KC_BTN2);
 			break;
@@ -187,23 +84,21 @@ void u_td_fn_btn2scrl_reset(tap_dance_state_t *state, void *user_data) {
 		default:
 			break;
     }
-	drg_state = TD_NONE;
+	td_state = TD_NONE;
 }
 
 // 더블탭 실행 액션 추가
 tap_dance_action_t tap_dance_actions[] = {
     [U_TD_BOOT] = ACTION_TAP_DANCE_FN(u_td_fn_boot),
-    [U_TD_COMM_SCRL] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, u_td_fn_commscrl_finish, u_td_fn_commscrl_reset),
+    [U_TD_SLSH_SCRL] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, u_td_fn_slsh_finish, u_td_fn_slsh_reset),
     [U_TD_BTN2_SCRL] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, u_td_fn_btn2scrl_finish, u_td_fn_btn2scrl_reset),
-	#define LAYER_X(LAYER, STRING) [U_TD_##LAYER] = ACTION_TAP_DANCE_FN(u_td_fn_##LAYER),
-	LAYER_LIST
-	#undef LAYER_X
 };
 
 //유저키코드
 static bool is_eclipse = false;
 static bool is_browser = false;
 static bool is_nexaro = false;
+
 // 유저키코드 처리
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
