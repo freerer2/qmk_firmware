0bcffedd7426adf0af27e4fdbfad701aea64aff1
LHK <30331140+freerer2@users.noreply.github.com>
2024-08-05 17:29:41 +0900

    key 6개 삭제 테스트

--- users/lazybones/keymap_helper/settings/combo_setting.h
+++ users/lazybones/keymap_helper/settings/combo_setting.h
@@ -8,6 +8,12 @@ COMBO_X(right_thumb,	KC_DEL,		LT(U_SYM,KC_ENT),	LT(U_NUM,KC_BSPC)) \
 COMBO_X(XC,				KC_UNDS,	KC_X,				KC_C) \
 COMBO_X(SD,				KC_ALGR,	LALT_T(KC_S),		LCTL_T(KC_D)) \
 COMBO_X(KL,				KC_CAPS,	LCTL_T(KC_K),		LALT_T(KC_L)) \
-COMBO_X(COMMDOT,		KC_B,		KC_COMM,			KC_DOT)
+COMBO_X(COMMDOT,		KC_B,		KC_COMM,			KC_DOT) \
+COMBO_X(ER,				KC_T,		KC_E,				KC_R) \
+COMBO_X(UI,				KC_Y,		KC_U,				KC_I) \
+COMBO_X(DF,				KC_G,		LCTL_T(KC_D),		LSFT_T(KC_F)) \
+COMBO_X(JK,				KC_H,		LSFT_T(KC_J),		LCTL_T(KC_K)) \
+COMBO_X(CV,				KC_B,		KC_C,				KC_V) \
+COMBO_X(MCOMM,			KC_N,		KC_M,				KC_COMM)
 
 //COMBO_X(SD,				S(KC_SPC),	LALT_T(KC_S),		LCTL_T(KC_D))
