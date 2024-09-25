c147746a2e7b6c71b4518908eb45d57517ce86b0
LHK <30331140+freerer2@users.noreply.github.com>
2024-09-25 17:32:31 +0900

    D

--- users/lazybones/keymap_helper/settings/combo_setting.h
+++ users/lazybones/keymap_helper/settings/combo_setting.h
@@ -2,11 +2,11 @@
 
 // 콤보 정의(이름, 작동키, 콤보키)
 #define COMBO_LIST \
-COMBO_X(mouse_thumb,		KC_BTN3,			KC_BTN1,			KC_BTN2) \
-COMBO_X(XC,					KC_UNDS,			KC_X,				KC_C) \
-COMBO_X(CV,					KC_ALGR,			KC_C,				KC_V) \
-COMBO_X(MCOMM,				KC_CAPS,			KC_M,				KC_COMM) \
-COMBO_X(COMMDOT,			KC_B,				KC_COMM,			KC_DOT) \
-COMBO_X(AS,					KC_ESC,				LGUI_T(KC_A),		LALT_T(KC_S)) \
-COMBO_X(LQUOT,				KC_DEL,				LALT_T(KC_L),		LGUI_T(KC_QUOT)) \
+COMBO_X(left_thumb,		KC_ESC,		LT(U_NAV,KC_SPC),	LT(U_FUN,KC_TAB)) \
+COMBO_X(mouse_thumb,	KC_BTN3,	KC_BTN1,			KC_BTN2) \
+COMBO_X(right_thumb,	KC_DEL,		LT(U_SYM,KC_ENT),	LT(U_NUM,KC_BSPC)) \
+COMBO_X(XC,				KC_UNDS,	KC_X,				KC_C) \
+COMBO_X(COMMDOT,	    KC_B,		KC_COMM,			KC_DOT) \
+COMBO_X(SD,				KC_ALGR,	LALT_T(KC_S),		LCTL_T(KC_D)) \
+COMBO_X(KL,				KC_CAPS,	LCTL_T(KC_K),		LALT_T(KC_L)) \
 
