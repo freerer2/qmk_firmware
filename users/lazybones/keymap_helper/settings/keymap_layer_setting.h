99c70c18fda02df1a3fae3ee2d01852140d97987
LHK <30331140+freerer2@users.noreply.github.com>
2024-10-14 16:01:41 +0900

    디지타이저 고도화

--- users/lazybones/keymap_helper/settings/keymap_layer_setting.h
+++ users/lazybones/keymap_helper/settings/keymap_layer_setting.h
@@ -6,13 +6,21 @@
 KC_Q,				KC_W,				KC_E,				KC_R,				KC_T,				KC_Y,				KC_U,				KC_I,				KC_O,				KC_P,				\
 LGUI_T(KC_A),		LALT_T(KC_S),		LCTL_T(KC_D),		LSFT_T(KC_F),		KC_G,				KC_H,				LSFT_T(KC_J),		LCTL_T(KC_K),		LALT_T(KC_L),		LGUI_T(KC_QUOT),	\
 LT(U_MOUSE,KC_Z),	KC_X,				KC_C,				KC_V,				KC_B,				KC_N,				KC_M,				KC_COMM,			KC_DOT,				TD(U_TD_SLSH_SCRL),	\
-										LT(U_DEBUG,KC_ESC),	LT(U_NAV,KC_SPC),	LT(U_FUN,KC_TAB),	LT(U_SYM,KC_ENT),	LT(U_NUM,KC_BSPC),	KC_DEL
+										LT(U_DEBUG,KC_ESC),	LT(U_NAV,KC_SPC),	LT(U_DIGI,KC_TAB),	LT(U_FUN,KC_ENT),	LT(U_NUM,KC_BSPC),	KC_DEL
+
+										// LT(U_DEBUG,KC_ESC),	LT(U_NAV,KC_SPC),	LT(U_FUN,KC_TAB),	LT(U_SYM,KC_ENT),	LT(U_NUM,KC_BSPC),	KC_DEL
 
 #define LAYER_NUM \
 KC_LBRC,			KC_7,				KC_8,				KC_9,				KC_RBRC,			KC_NO,				DF(U_BASE),			KC_NO,				KC_NO,				TD(U_TD_BOOT),		\
 KC_SCLN,			KC_4,				KC_5,				KC_6,				KC_EQL,				KC_NO,				KC_LSFT,			KC_LCTL,			KC_LALT,			KC_LGUI,			\
 KC_GRV,				KC_1,				KC_2,				KC_3,				KC_BSLS,			KC_NO,				DF(U_NUM),			DF(U_NAV),			KC_ALGR,			KC_RCTL,			\
 										KC_DOT,				KC_0,				KC_MINS,			KC_NO,				KC_NO,				KC_NO
+
+#define LAYER_FUN \
+KC_NO,				KC_F7,				KC_F8,				KC_F9,				KC_PSCR,			KC_NO,				KC_NO,				KC_NO,				KC_NO,				TD(U_TD_BOOT),		\
+KC_NO,				KC_F4,				KC_F5,				KC_F6,				KC_SCRL,			KC_NO,				KC_LSFT,			KC_LCTL,			KC_LALT,			KC_LGUI,			\
+KC_NO,				KC_F1,				KC_F2,				KC_F3,				KC_PAUS,			KC_NO,				KC_NO,				KC_NO,				KC_ALGR,			KC_RCTL,			\
+										KC_F10,				KC_F11,				KC_F12,				KC_NO,				KC_NO,				KC_NO
 
 #define LAYER_SYM \
 KC_LCBR,			KC_AMPR,			KC_ASTR,			KC_LPRN,			KC_RCBR,			KC_NO,				DF(U_BASE),			KC_NO,				KC_NO,				TD(U_TD_BOOT),		\
@@ -26,16 +34,22 @@ KC_LGUI,			KC_LALT,			KC_LCTL,			KC_LSFT,			KC_NO,				KC_CAPS,			KC_LEFT,			KC_D
 KC_RCTL,			KC_ALGR,			DF(U_NUM),			DF(U_NAV),			KC_NO,				U_RDO,				U_PST,				U_CPY,				U_CUT,				U_UND,				\
 										KC_NO,				KC_NO,				KC_NO,				KC_ENT,				KC_BSPC,			KC_DEL
 
-#define LAYER_FUN \
-TD(U_TD_BOOT),		KC_NO,				KC_NO,				DF(U_BASE),			KC_NO,				KC_PSCR,			KC_F7,				KC_F8,				KC_F9,				KC_F12,				\
-KC_LGUI,			KC_LALT,			KC_LCTL,			KC_LSFT,			KC_NO,				KC_SCRL,			KC_F4,				KC_F5,				KC_F6,				KC_F11,				\
-KC_RCTL,			KC_ALGR,			DF(U_SYM),			DF(U_FUN),			KC_NO,				KC_PAUS,			KC_F1,				KC_F2,				KC_F3,				KC_F10,				\
-										KC_NO,				KC_NO,				KC_NO,				KC_ENT,				KC_BSPC,			KC_APP
+// #define LAYER_FUN
+//TD(U_TD_BOOT),		KC_NO,				KC_NO,				DF(U_BASE),			KC_NO,				KC_PSCR,			KC_F7,				KC_F8,				KC_F9,				KC_F12,				
+//KC_LGUI,			KC_LALT,			KC_LCTL,			KC_LSFT,			KC_NO,				KC_SCRL,			KC_F4,				KC_F5,				KC_F6,				KC_F11,				
+//KC_RCTL,			KC_ALGR,			DF(U_SYM),			DF(U_FUN),			KC_NO,				KC_PAUS,			KC_F1,				KC_F2,				KC_F3,				KC_F10,				
+//										KC_NO,				KC_NO,				KC_NO,				KC_ENT,				KC_BSPC,			KC_APP
 
 #define LAYER_DEBUG \
 DM_REC1,			DM_PLY1,			RESUME,				DM_PLY2,			DM_REC2,			KC_NO,				KC_NO,				KC_NO,				KC_NO,				KC_NO,				\
 KC_NO,			    STEP_INTO,			STEP_OVER,			STEP_OUT,			KC_NO,				KC_NO,				KC_NO,				KC_NO,				KC_NO,				KC_NO,				\
 KC_NO,				ECLIPSE,			BROWSER,			NEXACRO,			KC_NO,				KC_NO,				KC_NO,				KC_NO,				KC_NO,				KC_NO,				\
+										KC_NO,				KC_NO,				KC_NO,				KC_NO,				KC_NO,				KC_NO
+
+#define LAYER_DIGI \
+B_A_A,				B_B_A,				B_C_A,				B_D_A,				B_E_A,				B_F_A,				B_G_A,				B_H_A,				B_I_A,				B_J_A,				\
+B_A_B,			    B_B_B,				B_C_B,				B_D_B,				B_E_B,				B_F_B,				B_G_B,				B_H_B,				B_I_B,				B_J_B,				\
+B_A_C,				B_B_C,				B_C_C,				B_D_C,				B_E_C,				B_F_C,				B_G_C,				B_H_C,				B_I_C,				B_J_C,				\
 										KC_NO,				KC_NO,				KC_NO,				KC_NO,				KC_NO,				KC_NO
 
 #ifdef POINTING_DEVICE_ENABLE
@@ -45,10 +59,10 @@ KC_NO,				ECLIPSE,			BROWSER,			NEXACRO,			KC_NO,				KC_NO,				KC_NO,				KC_NO,	
 	KC_NO,				U_CUT,				U_CPY,				U_PST,				C(KC_A),			U_NEW_DSKT,			KC_BTN4,			KC_BTN5,			SNIPING,			U_DEL_DSKT,			\
 											KC_BTN3,			KC_BTN1,			TD(U_TD_BTN2_SCRL),	KC_BTN2,			KC_BTN1,			KC_BTN3
 	#define LAYER_SCROL \
-	A_A,				B_A,				C_A,				D_A,				E_A,				KC_NO,				KC_NO,				KC_NO,				KC_NO,				KC_NO,				\
-	A_B,				B_B,				C_B,				D_B,				E_B,				KC_NO,				KC_NO,				KC_NO,				KC_NO,				KC_NO,				\
-	A_C,				B_C,				C_C,				D_C,				E_C,				KC_NO,				KC_BTN4,			KC_BTN5,			SNIPING,			KC_NO,				\
-											KC_NO,				KC_NO,				KC_NO,				KC_BTN2,			KC_BTN1,			KC_BTN3
+	KC_NO,				G(KC_7),			G(KC_8),			G(KC_9),			KC_NO,				KC_NO,				KC_NO,				KC_NO,				KC_NO,				KC_NO,				\
+	KC_NO,				G(KC_4),			G(KC_5),			G(KC_6),			KC_NO,				KC_NO,				KC_NO,				KC_NO,				KC_NO,				KC_NO,				\
+	KC_NO,				G(KC_1),			G(KC_2),			G(KC_3),			KC_NO,				KC_NO,				KC_BTN4,			KC_BTN5,			SNIPING,			KC_NO,				\
+											KC_NO,				G(KC_0),			KC_NO,				KC_BTN2,			KC_BTN1,			KC_BTN3
 #else
 	#define LAYER_MOUSE \
 	U_UND,				C(KC_W),			C(KC_E),			C(S(KC_R)),			U_RDO,				KC_NO,				U_WINFLT,			KC_MS_U,			U_WINFRT,			KC_NO,				\
@@ -56,10 +70,10 @@ KC_NO,				ECLIPSE,			BROWSER,			NEXACRO,			KC_NO,				KC_NO,				KC_NO,				KC_NO,	
 	KC_NO,				U_CUT,				U_CPY,				U_PST,				C(KC_A),			U_NEW_DSKT,			KC_BTN4,			KC_BTN5,			KC_NO,				U_DEL_DSKT,			\
 											KC_BTN3,			KC_BTN1,			LSFT_T(KC_BTN2),	LSFT_T(KC_BTN2),	KC_BTN1,			KC_BTN3
 	#define LAYER_SCROL \
-	A_A,				B_A,				C_A,				D_A,				E_A,				KC_NO,				U_WINFLT,			KC_MS_U,			U_WINFRT,			KC_NO,				\
-	A_B,				B_B,				C_B,				D_B,				E_B,				U_LFT_DSKT,			KC_MS_L,			KC_MS_D,			KC_MS_R,			U_RGT_DSKT,			\
-	A_C,				B_C,				C_C,				D_C,				E_C,				U_NEW_DSKT,			KC_BTN4,			KC_BTN5,			KC_NO,				U_DEL_DSKT,			\
-											KC_NO,				KC_NO,				KC_NO,				KC_BTN2,			KC_BTN1,			KC_BTN3
+	KC_NO,				G(KC_7),			G(KC_8),			G(KC_9),			KC_NO,				KC_NO,				U_WINFLT,			KC_MS_U,			U_WINFRT,			KC_NO,				\
+	KC_NO,				G(KC_4),			G(KC_5),			G(KC_6),			KC_NO,				U_LFT_DSKT,			KC_MS_L,			KC_MS_D,			KC_MS_R,			U_RGT_DSKT,			\
+	KC_NO,				G(KC_1),			G(KC_2),			G(KC_3),			KC_NO,				U_NEW_DSKT,			KC_BTN4,			KC_BTN5,			KC_NO,				U_DEL_DSKT,			\
+											KC_NO,				G(KC_0),			KC_NO,				KC_BTN2,			KC_BTN1,			KC_BTN3
 #endif // POINTING_DEVICE_ENABLE
 
 // 레이어 정의
@@ -71,4 +85,5 @@ LAYER_X(FUN,	"Fun") \
 LAYER_X(SYM,	"Sym") \
 LAYER_X(MOUSE,	"Mouse") \
 LAYER_X(SCROL,	"Scroll") \
-LAYER_X(DEBUG,	"Debug")
+LAYER_X(DEBUG,	"Debug") \
+LAYER_X(DIGI,	"Digitizer")
